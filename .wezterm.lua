5e4fe212580ccbb5ba08656aeaaf8517ffce1bd3
pick3t <pick3t@126.com>
2025-01-17 18:27:22 +0800

    wezterm: explicitly disable scroll bar, enable cursive italic

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,11 +7,22 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 -- For example, changing the color scheme:
 config.color_scheme = 'AdventureTime'
-config.font = wezterm.font('OperatorMono Nerd Font', { weight = 'ExtraBold' })
+config.font = wezterm.font(
+    {
+        family = 'OperatorMono Nerd Font',
+        -- this option enables cursive italic for some fonts, e.g. Cascadia Code/Mono NF/PL
+        harfbuzz_features = {
+            "ss01"
+        }
+    },
+    {
+        weight = 'ExtraBold'
+    })
 config.font_size = 17.0
 
 config.hide_tab_bar_if_only_one_tab = true
 
+config.enable_scroll_bar = false
 config.window_padding = {
     left = 0,
     right = 0,
